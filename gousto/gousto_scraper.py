01b2ecea7acab34a335aed46547f3937bbade16e
francesca sandford <fran6san@gmail.com>
2024-03-24 16:15:02 +0000

    feat: generate recipes as markdown from gousto

---
+++ gousto/gousto_scraper.py
@@ -0,0 +1,35 @@
+from recipe_scrapers import scrape_me
+import sys
+
+def generate_markdown(scraper):
+    markdown = f"# {scraper.title()}\n\n"
+
+    if scraper.total_time():
+        markdown += f"**Cooking time**: {scraper.total_time()} mins\n\n"
+
+    if scraper.ingredients():
+        markdown += "## Ingredients:\n\n"
+        for ingredient in scraper.ingredients():
+            markdown += f"- {ingredient}\n"
+        markdown += "\n"
+
+    if scraper.instructions():
+        markdown += "## Instructions:\n\n"
+        markdown += scraper.instructions()
+
+    return markdown
+
+def write_to_file(recipe, filename):
+    with open(filename, "w") as file:
+        file.write(recipe)
+    print(f"Recipe: '{filename}' has been generated")
+
+recipe_url = sys.argv[1]
+scraper = scrape_me(f"https://www.gousto.co.uk/cookbook/{recipe_url}")
+
+markdown_content = generate_markdown(scraper)
+
+cleaned_recipe_title = scraper.title().replace(":", " ")
+filename = f"{cleaned_recipe_title}.md"
+
+write_to_file(markdown_content, filename)
